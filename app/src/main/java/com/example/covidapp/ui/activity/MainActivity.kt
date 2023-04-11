f756cb5cbb7098c3f54cdbeb99fcf87335e5513c
Yash Shirish Mehta <yashmehta2711@gmail.com>
2023-04-10 23:32:08 -0700

    Completed Lab 3 COVID App

--- app/src/main/java/com/example/covidapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/covidapp/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.covidapp.ui.activity
 
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -107,26 +108,43 @@ class MainActivity : AppCompatActivity() {
             goNextScreen(SymptomsActivity::class.java)
         }
 
-        btnKnowMore.setOnClickListener {
-            loadWeb(KNOW_MORE)
-        }
-
-        btnWebsite.setOnClickListener {
-            loadWeb(SWAPNIL_WEB)
-        }
-
-        btnGithub.setOnClickListener {
-            loadWeb(SWAPNIL_GITHUB)
-        }
-
-        btnLinkedIn.setOnClickListener {
-            loadWeb(SWAPNIL_LINKEDIN)
-        }
 
         txtViewPrecautions.setOnClickListener {
             goNextScreen(PrecautionActivity::class.java)
         }
     }
+    override fun onBackPressed() {
+        // Create the object of AlertDialog Builder class
+        val builder = AlertDialog.Builder(this)
+
+        // Set the message show for the Alert time
+        builder.setMessage("Do you want to exit ?")
+
+        // Set Alert Title
+        builder.setTitle("Alert !")
+
+        // Set Cancelable false for when the user clicks on the outside the Dialog Box then it will remain show
+        builder.setCancelable(false)
+
+        // Set the positive button with yes name Lambda OnClickListener method is use of DialogInterface interface.
+        builder.setPositiveButton("Yes") {
+            // When the user click yes button then app will close
+                dialog, which -> finish()
+        }
+
+        // Set the Negative button with No name Lambda OnClickListener method is use of DialogInterface interface.
+        builder.setNegativeButton("No") {
+            // If user click no then dialog box is canceled.
+                dialog, which -> dialog.cancel()
+        }
+
+        // Create the Alert dialog
+        val alertDialog = builder.create()
+        // Show the Alert Dialog box
+        alertDialog.show()
+    }
+
+
 
     private fun getGlobalData() = binding.run {
         val url: String = "https://corona.lmao.ninja/v2/all/"
